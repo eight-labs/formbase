3a2e3c4b70a8c209afe3ca958ef863795e63ee45
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-02-19 01:08:29 +0000

    feat: add middleware to rewrite from /s/to /api/s/

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,8 +7,18 @@ export async function middleware(request: NextRequest): Promise<NextResponse> {
   if (request.method === "GET") {
     return NextResponse.next();
   }
+
   const originHeader = request.headers.get("Origin");
   const hostHeader = request.headers.get("Host");
+  const path = request.nextUrl.pathname;
+  const url = request.nextUrl as unknown as URL;
+
+  if (path.match("^/s/([a-zA-Z0-9]+)$")) {
+    const subpath = path.split("/")[path.split("/").length - 1];
+
+    return NextResponse.rewrite(new URL(`/api/s/${subpath}`, url));
+  }
+
   if (
     !originHeader ||
     !hostHeader ||
@@ -18,6 +28,7 @@ export async function middleware(request: NextRequest): Promise<NextResponse> {
       status: 403,
     });
   }
+
   return NextResponse.next();
 }
 
