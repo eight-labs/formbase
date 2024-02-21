46fe4521c7bddabf7a5aeffc588833ca6c543f64
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-02-21 13:54:41 +0000

    chore: minor landing page improvements

--- src/app/(landing)/_components/header.tsx
+++ src/app/(landing)/_components/header.tsx
@@ -1,55 +1,31 @@
 import Link from "next/link";
-import { RocketIcon } from "~/components/icons";
 import { APP_TITLE } from "~/lib/constants";
 import { Button } from "~/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "~/components/ui/dropdown-menu";
-import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { FunctionSquare } from "lucide-react";
+import type { User as LuciaUser } from "lucia";
 
 const routes = [
   { name: "Home", href: "/" },
   { name: "Features", href: "/#features" },
-  {
-    name: "Documentation",
-    href: "https://www.touha.dev/posts/simple-nextjs-t3-authentication-with-lucia",
-  },
 ] as const;
 
-export const Header = () => {
+type LandingHeaderProps = {
+  user: LuciaUser | null;
+};
+
+export const Header = ({ user }: LandingHeaderProps) => {
+  const isLoggedIn = user !== null;
+
   return (
     <header className="px-2 py-4 lg:py-6">
       <div className="container flex items-center gap-2 p-0">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button
-              className="focus:outline-none focus:ring-1 md:hidden"
-              size="icon"
-              variant="outline"
-            >
-              <HamburgerMenuIcon className="h-5 w-5" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="start">
-            <div className="py-1">
-              {routes.map(({ name, href }) => (
-                <DropdownMenuItem key={name} asChild>
-                  <Link href={href}>{name}</Link>
-                </DropdownMenuItem>
-              ))}
-            </div>
-          </DropdownMenuContent>
-        </DropdownMenu>
         <Link
-          className="flex items-center justify-center text-xl font-medium"
-          href="/"
+          className="text flex items-center justify-center font-medium"
+          href={isLoggedIn ? "/dashboard" : "/"}
         >
-          <RocketIcon className="mr-2 h-5 w-5" /> {APP_TITLE}
+          <FunctionSquare className="mr-2 h-5 w-5" /> {APP_TITLE}
         </Link>
-        <nav className="ml-10 hidden gap-4 sm:gap-6 md:flex">
+        <nav className="ml-8 hidden gap-4 sm:gap-6 md:flex">
           {routes.map(({ name, href }) => (
             <Link
               key={name}
@@ -61,9 +37,15 @@ export const Header = () => {
           ))}
         </nav>
         <div className="ml-auto">
-          <Button asChild variant={"secondary"}>
-            <Link href="/login">Login</Link>
-          </Button>
+          {isLoggedIn ? (
+            <Button asChild variant={"secondary"}>
+              <Link href="/dashboard">Dashboard</Link>
+            </Button>
+          ) : (
+            <Button asChild variant={"secondary"}>
+              <Link href="/login">Login</Link>
+            </Button>
+          )}
         </div>
       </div>
     </header>
--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -2,16 +2,19 @@ import { APP_TITLE } from "~/lib/constants";
 import { type ReactNode } from "react";
 import { type Metadata } from "next";
 import { Header } from "./_components/header";
+import { validateRequest } from "~/lib/auth/validate-request";
 
 export const metadata: Metadata = {
   title: APP_TITLE,
   description: "Manage forms with ease",
 };
 
-function LandingPageLayout({ children }: { children: ReactNode }) {
+async function LandingPageLayout({ children }: { children: ReactNode }) {
+  const { user } = await validateRequest();
+
   return (
     <>
-      <Header />
+      <Header user={user} />
       {children}
     </>
   );
