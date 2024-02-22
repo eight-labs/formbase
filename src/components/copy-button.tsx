033c6d1569ef1a8aaa8d0f9e49f400c36b2dc7ad
AmoabaKelvin <kel.amoaba@gmail.com>
2024-02-22 00:05:40 +0000

    prevent default behaviour when the copy button is clicked

--- src/components/copy-button.tsx
+++ src/components/copy-button.tsx
@@ -11,7 +11,8 @@ export const CopyButton = ({ text }: CopyButtonProps) => {
   return (
     <Copy
       className="h-4 w-4 cursor-pointer text-muted-foreground transition-transform hover:scale-110 hover:transform"
-      onClick={async () => {
+      onClick={async (e) => {
+        e.preventDefault();
         await navigator.clipboard.writeText(text);
         toast("Copied to clipboard", {
           icon: <Copy className="h-4 w-4" />,
