1efbd4d1c8d3e16ae3388dfa6e9d49a02cfe74d2
AmoabaKelvin <kel.amoaba@gmail.com>
2024-02-26 00:38:29 +0000

    fix: update keys column to text array

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -109,7 +109,7 @@ export const forms = createTable(
     sendEmailForNewSubmissions: boolean("send_email_for_new_submissions")
       .default(true)
       .notNull(),
-    keys: varchar("keys").notNull(),
+    keys: text("keys").array().notNull(),
   },
   (t) => ({
     userIdx: index("form_user_idx").on(t.userId),
