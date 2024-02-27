2c89c86c29270afcbb6ec28a4f05441941375f38
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-02-27 21:53:24 +0000

    feat: user profile settings ui

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -30,7 +30,7 @@ export default async function DashboardPage() {
     <div className="py-10 md:py-8">
       <div className="mb-10 flex w-full items-end justify-between">
         <div className="flex flex-col gap-1">
-          <h1 className="text-3xl font-bold">Forms</h1>
+          <h1 className="text-3xl font-medium">Forms</h1>
           <p className="text-sm text-muted-foreground">
             Manage your forms here
           </p>
---
+++ src/app/(main)/dashboard/settings/profile-form.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+
+const profileFormSchema = z.object({
+  email: z
+    .string({
+      required_error: "Please select an email to display.",
+    })
+    .email(),
+  name: z.string(),
+});
+
+type ProfileFormValues = z.infer<typeof profileFormSchema>;
+
+// This can come from your database or API.
+const defaultValues: Partial<ProfileFormValues> = {
+  name: "Steve Jobs",
+  email: "steve@apple.com",
+};
+
+export function ProfileForm() {
+  const form = useForm<ProfileFormValues>({
+    resolver: zodResolver(profileFormSchema),
+    defaultValues,
+    mode: "onChange",
+  });
+
+  function onSubmit(data: ProfileFormValues) {
+    toast("You submitted the following values", {
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Steve Jobs"
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="steve@apple.com" disabled {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Update Profile</Button>
+      </form>
+    </Form>
+  );
+}
