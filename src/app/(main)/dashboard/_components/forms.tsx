af08c7e030247acdcee470cffe1b19d2b94d4224
AmoabaKelvin <kel.amoaba@gmail.com>
2024-03-03 15:34:17 +0000

    pass form details to form card and not make an extra trip to get them
    this reduces the number of api calls needed to render the form details on the card thereby reducing database read rates.

--- src/app/(main)/dashboard/_components/forms.tsx
+++ src/app/(main)/dashboard/_components/forms.tsx
@@ -20,6 +20,10 @@ export function Forms({ promises }: FormsProps) {
    */
   const [forms, subscriptionPlan] = use(promises);
 
+  console.log(forms);
+
+  console.log(forms[0]?.formData.length);
+
   /**
    * useOptimistic is a React Hook that lets you show a different state while an async action is underway.
    * It accepts some state as an argument and returns a copy of that state that can be different during the duration of an async action such as a network request.
@@ -31,7 +35,13 @@ export function Forms({ promises }: FormsProps) {
       {forms.length > 0 ? (
         <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
           {forms.map((form) => (
-            <FormCard form={form} key={form.id} />
+            <FormCard
+              form={form}
+              key={form.id}
+              submissionsCount={form.formData.length}
+              updatedAt={form.updatedAt}
+              createdAt={form.createdAt}
+            />
           ))}
         </div>
       ) : (
