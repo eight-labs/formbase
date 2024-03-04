02798e013d3d43527961111d1b13ce63236dd317
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-03-04 05:58:48 +0000

    feat: new and better icons on the landing page

--- src/app/(landing)/_components/theme-toggle.tsx
+++ src/app/(landing)/_components/theme-toggle.tsx
@@ -18,7 +18,11 @@ export function ThemeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="sm" className="h-8 w-8 px-0">
+        <Button
+          variant="ghost"
+          size="sm"
+          className="h-5 w-5 p-2 px-0 outline-none focus:ring-0"
+        >
           <Sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
@@ -26,15 +30,15 @@ export function ThemeToggle() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          <Sun className="mr-2 h-3 w-3" />
+          <Sun className="mr-2 h-4 w-4" />
           <span>Light</span>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          <Moon className="mr-2 h-3 w-3" />
+          <Moon className="mr-2 h-4 w-4" />
           <span>Dark</span>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
-          <Laptop className="mr-2 h-3 w-3" />
+          <Laptop className="mr-2 h-4 w-4" />
           <span>System</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
