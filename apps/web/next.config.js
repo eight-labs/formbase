c47cb8f67964d8d131377eafe72d69ed0e3024ff
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-04-30 13:36:10 +0000

    chore: remove unused files

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import path from "path";
 import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
