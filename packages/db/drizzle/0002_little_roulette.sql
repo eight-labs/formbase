d654715d43e2565eba5ec10a89ecd520b34a34dc
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-06-05 04:01:32 +0000

    chore: reset migrations

--- packages/db/drizzle/0002_little_roulette.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE "user";--> statement-breakpoint
-DROP TABLE "session";--> statement-breakpoint
-ALTER TABLE "oauth_account" DROP CONSTRAINT "oauth_account_user_id_user_id_fk";
---> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "oauth_account" ADD CONSTRAINT "oauth_account_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
