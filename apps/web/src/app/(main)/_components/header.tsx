95c383894aa67192d0d33b4e3a9316bb2ea478d0
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-06-07 16:47:45 +0000

    fix: move link from header to dropdown

--- apps/web/src/app/(main)/_components/header.tsx
+++ apps/web/src/app/(main)/_components/header.tsx
@@ -6,10 +6,7 @@ import { type LuciaUser } from '@formbase/auth';
 
 import { UserDropdown } from './user-dropdown';
 
-const routes = [
-  { name: 'Dashboard', href: '/dashboard' },
-  { name: 'Onboarding', href: '/onboarding' },
-] as const;
+const routes = [{ name: 'Dashboard', href: '/dashboard' }] as const;
 
 export const Header = ({ user }: { user: LuciaUser }) => {
   return (
